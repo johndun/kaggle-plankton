ef51eabf8e3a84709f1a9889d2d2adfb4f9b4386
Ubuntu <ubuntu@ip-172-31-32-231.ec2.internal>
2015-02-02 04:20:59 +0000

    cleaning

--- log/convnet5.lua
+++
@@ -1,7 +0,0 @@
-[0m### Early stopping using validation set[0m	
-[0m### Calculating parameters for global contrast normalization[0m	
-[0m
-### Training at learning rate 1: 1[0m	
-[0m### Number of model parameters: 10540537[0m	
-[0m
-Training epoch 1[0m	
